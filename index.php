6c090478ea18ba467f2e478651bf75323bd7ce8c
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-03-30 10:10:14 +0100

    get location and temperature

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 <script src="js/jquery.js"></script>
 <script src="js/script.js"></script>
 <p>weatherboy</p>
+<p id="location"></p>
+<p id="temperature"></p>
 
 <input id="temperature">
--- js/script.js
+++ js/script.js
@@ -6,14 +6,16 @@ $(document).ready(function() {
         latitude = json.coord.lat;
     }
 
-
     var latitude = 54.321475;
     var longitude = -6.306556;
     var appid = '86afc7330041b7b874026b441fdf564e';
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + appid, function (data) {
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=" + appid, function (data) {
     var rawJson = JSON.stringify(data);
     var json = JSON.parse(rawJson);
     updateWeather(json); //Update Weather parameters
+    $('#temperature').html(json.main.temp);
+    $('#location').html(json.name);
+
 });
 
 

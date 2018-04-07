3e97e9aed7963286bbaa631392235024fe9dbabd
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-07 16:34:05 +0100

    updating paths

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/weather-app/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="/weather-app/js/script.js"></script>
+    <script src="js/script.js"></script>
     <title>Weather App</title>
 </head>
 <body>

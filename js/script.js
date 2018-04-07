65251be4253feb72430bc8c8724ee691260e956e
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-07 13:42:57 +0100

    setting delay on search

--- js/script.js
+++ js/script.js
@@ -9,16 +9,21 @@ $(document).ready(function () {
     var latitude = 54.321475;
     var longitude = -6.306556;
     var appid = '86afc7330041b7b874026b441fdf564e';
+    var timer = null;
 
     $('#location-search').on('keyup', function () {
         var location = $(this).val();
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + appid, function (data) {
-            var rawJson = JSON.stringify(data);
-            var json = JSON.parse(rawJson);
-            $('#temperature').html(Math.round(json.main.temp));
-            $('#location').html(json.name);
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + appid, function (data) {
+                var rawJson = JSON.stringify(data);
+                var json = JSON.parse(rawJson);
+                $('#temperature').html(Math.round(json.main.temp));
+                $('#location').html(json.name);
+                console.log(json);
+            });
+        }, 2000);
 
-        });
     });
 
     $( "#compare" ).submit(function() {

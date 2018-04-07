7f05b95e34d16286476ea4a8b20150ad091d1d6d
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-07 14:53:59 +0100

    adding bootstrap

--- compare.php
+++ compare.php
@@ -1,26 +1,34 @@
 <?php
 include_once('includes/header.php');
 ?>
+    <div class="container">
+        <form class="form" id="compare">
+            <p>compare weather</p>
+            <input id="location-compare-1" type="text" name="location-1" placeholder="search for your weather">
+            <input id="location-compare-2" type="text" name="location-2" placeholder="search for your weather">
+            <input type="submit" value="compareo">
+        </form>
 
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 weather-output">
+                <h3 class="weather"><span id="location1"></span></h3>
+                <h2 class="weather"><span id="temperature1"></span>°C</h2>
+                <p class="weather"><span id="forecast1"></span></p>
+                <p class="weather">High: <span id="high-temp1"></span>°C</p>
+                <p class="weather">Low: <span id="low-temp1"></span>°C</p>
+            </div>
 
-<form class="form" id="compare">
-<p>compare weather</p>
-    <input id="location-compare-1" type="text" name="location-1" placeholder="search for your weather">
-    <input id="location-compare-2" type="text" name="location-2" placeholder="search for your weather">
-    <input type="submit" value="compareo">
-</form>
+            <div class="col-xs-12 col-sm-6 weather-output">
+                <h3 class="weather"><span id="location2"></span></h3>
+                <h2 class="weather"><span id="temperature2"></span>°C</h2>
+                <p class="weather"><span id="forecast2"></span></p>
+                <p class="weather">High: <span id="high-temp2"></span>°C</p>
+                <p class="weather">Low: <span id="low-temp2"></span>°C</p>
+            </div>
+        </div>
 
-<div class="half">
-    <p id="location1"></p>
-    <p id="temperature1"></p>
-</div>
-
-<div class="half">
-    <p id="location2"></p>
-    <p id="temperature2"></p>
-</div>
-
-<a href="single.php">Search Weather</a>
+        <a href="single.php">Search Weather</a>
+    </div>
 
 <?php
 include_once('includes/footer.php');
--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="/weather-app/css/style.css" />
-    <script src="/weather-app/js/jquery.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="/weather-app/js/script.js"></script>
     <title>weatherboy</title>
 </head>

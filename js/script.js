fafc8bca6dd8e5d88851783e296cbf8a31a2ec53
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-07 14:54:32 +0100

    reworking single search form

--- js/script.js
+++ js/script.js
@@ -3,26 +3,23 @@ $(document).ready(function () {
     var latitude = 54.321475;
     var longitude = -6.306556;
     var appid = '86afc7330041b7b874026b441fdf564e';
-    var timer = null;
 
     //single weather search
-    $('#location-search').on('keyup', function () {
-        var location = $(this).val();
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + appid, function (data) {
-                var rawJson = JSON.stringify(data);
-                var json = JSON.parse(rawJson);
-                $('#temperature').html(Math.round(json.main.temp));
-                $('#location').html(json.name);
-                $('#high-temp').html(json.main.temp_max);
-                $('#low-temp').html(json.main.temp_min);
-                $('#forecast').html(json.weather[0].description);
-                $('.weather').css({"display":"block"});
-                console.log(json);
-            });
-        }, 2000);
+    $('#search').submit(function () {
+        var location = $('#search #location-search').val();
 
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&APPID=" + appid, function (data) {
+            var rawJson = JSON.stringify(data);
+            var json = JSON.parse(rawJson);
+            $('#temperature').html(Math.round(json.main.temp));
+            $('#location').html(json.name);
+            $('#high-temp').html(json.main.temp_max);
+            $('#low-temp').html(json.main.temp_min);
+            $('#forecast').html(json.weather[0].description);
+            $('.weather').css({"display": "block"});
+            console.log(json);
+        });
+return false;
     });
 
     //comparing weather search
--- single.php
+++ single.php
@@ -1,13 +1,27 @@
 <?php
 include_once('includes/header.php');
 ?>
-<input id="location-search" type="search" name="city" placeholder="search for your weather">
+    <div class="container">
+        <form class="form" id="search">
+            <p>Search for a location below</p>
+            <input id="location-search" type="search" name="city" placeholder="search for your weather">
+            <input type="submit" value="Search">
+        </form>
 
-<p class="weather" style="display: none">It's currently <span id="temperature"></span>°C with a high of <span id="high-temp"></span>°C and a low of <span id="low-temp"></span>°C</p>
+        <div class="row">
 
-
-<a href="compare.php">Compare Weather</a>
-
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="weather-output">
+                    <h3 class="weather"><span id="location"></span></h3>
+                    <h2 class="weather"><span id="temperature"></span>°C</h2>
+                    <p class="weather"><span id="forecast"></span></p>
+                    <p class="weather">High: <span id="high-temp"></span>°C</p>
+                    <p class="weather">Low: <span id="low-temp"></span>°C</p>
+                </div>
+            </div>
+        </div>
+        <a href="compare.php">Compare weather in two locations</a>
+    </div>
 
 <?php
 include_once('includes/footer.php');

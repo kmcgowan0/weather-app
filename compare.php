49b704a65d161041718680bc8abccb53596a578c
Katie Mcgowan <katiemcgoo@hotmail.co.uk>
2018-04-07 15:48:50 +0100

    fixing error reporting on compare

--- compare.php
+++ compare.php
@@ -13,21 +13,22 @@ include_once('includes/header.php');
 
             </div>
             <div class="col-xs-12 col-sm-6 weather-output">
-                <h3 class="weather"><span id="location1"></span></h3>
-                <h2 class="weather"><span id="temperature1"></span>°C</h2>
-                <p class="weather"><span id="forecast1"></span></p>
-                <p class="weather">High: <span id="high-temp1"></span>°C</p>
-                <p class="weather">Low: <span id="low-temp1"></span>°C</p>
+                <h3 class="weather weather1"><span id="location1"></span></h3>
+                <h2 class="weather weather1"><span id="temperature1"></span>°C</h2>
+                <p class="weather weather1"><span id="forecast1"></span></p>
+                <p class="weather weather1">High: <span id="high-temp1"></span>°C</p>
+                <p class="weather weather1">Low: <span id="low-temp1"></span>°C</p>
+                <p class="error1">Sorry, that location could not be found, please check your spelling and try searching again.</p>
             </div>
 
             <div class="col-xs-12 col-sm-6 weather-output">
-                <h3 class="weather"><span id="location2"></span></h3>
-                <h2 class="weather"><span id="temperature2"></span>°C</h2>
-                <p class="weather"><span id="forecast2"></span></p>
-                <p class="weather">High: <span id="high-temp2"></span>°C</p>
-                <p class="weather">Low: <span id="low-temp2"></span>°C</p>
+                <h3 class="weather weather2"><span id="location2"></span></h3>
+                <h2 class="weather weather2"><span id="temperature2"></span>°C</h2>
+                <p class="weather weather2"><span id="forecast2"></span></p>
+                <p class="weather weather2">High: <span id="high-temp2"></span>°C</p>
+                <p class="weather weather2">Low: <span id="low-temp2"></span>°C</p>
+                <p class="error2">Sorry, that location could not be found, please check your spelling and try searching again.</p>
             </div>
-            <p class="error">Sorry, one of those locations could not be found, please check your spelling and try searching again.</p>
         </div>
         <div class="col-xs-12 button-container">
             <a class="button" href="single.php">Search weather in one location</a>
--- js/script.js
+++ js/script.js
@@ -18,8 +18,14 @@ $(document).ready(function () {
             $('#forecast').html(json.weather[0].description);
 
         })
-            .done(function() { $('.error').css({"display": "none"}); $('.weather').css({"display": "block"});})
-            .fail(function() { $('.error').css({"display": "block"}); $('.weather').css({"display": "none"});});
+            .done(function () {
+                $('.error').css({"display": "none"});
+                $('.weather').css({"display": "block"});
+            })
+            .fail(function () {
+                $('.error').css({"display": "block"});
+                $('.weather').css({"display": "none"});
+            });
         return false;
     });
 
@@ -27,20 +33,24 @@ $(document).ready(function () {
     $("#compare").submit(function () {
         var location1 = $('#compare #location-compare-1').val();
         var location2 = $('#compare #location-compare-2').val();
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location1 + "&units=metric&APPID=" + appid, function (data) {
-                    var rawJson = JSON.stringify(data);
-                    var json = JSON.parse(rawJson);
-                    $('#temperature1').html(Math.round(json.main.temp));
-                    $('#location1').html(json.name);
-                    $('#high-temp1').html(json.main.temp_max);
-                    $('#low-temp1').html(json.main.temp_min);
-                    $('#forecast1').html(json.weather[0].description);
-
-                    // $('.error').css({"display": "block"});
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location1 + "&units=metric&APPID=" + appid, function (data) {
+            var rawJson = JSON.stringify(data);
+            var json = JSON.parse(rawJson);
+            $('#temperature1').html(Math.round(json.main.temp));
+            $('#location1').html(json.name);
+            $('#high-temp1').html(json.main.temp_max);
+            $('#low-temp1').html(json.main.temp_min);
+            $('#forecast1').html(json.weather[0].description);
+        })
+            .done(function () {
+                $('.error1').css({"display": "none"});
+                $('.weather1').css({"display": "block"});
             })
-                 .done(function() { $('.error').css({"display": "none"}); $('.weather').css({"display": "block"}); })
-                .fail(function() { $('.error').css({"display": "block"}); $('.weather').css({"display": "none"});});
-                // .always(function() { alert("complete"); });
+            .fail(function () {
+                $('.error1').css({"display": "block"});
+                $('.weather1').css({"display": "none"});
+            });
+        // .always(function() { alert("complete"); });
         $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + location2 + "&units=metric&APPID=" + appid, function (data) {
             var rawJson = JSON.stringify(data);
             var json = JSON.parse(rawJson);
@@ -50,8 +60,14 @@ $(document).ready(function () {
             $('#low-temp2').html(json.main.temp_min);
             $('#forecast2').html(json.weather[0].description);
         })
-            .done(function() { $('.error').css({"display": "none"}); $('.weather').css({"display": "block"}); })
-            .fail(function() { $('.error').css({"display": "block"}); $('.weather').css({"display": "none"}); });
+            .done(function () {
+                $('.error2').css({"display": "none"});
+                $('.weather2').css({"display": "block"});
+            })
+            .fail(function () {
+                $('.error2').css({"display": "block"});
+                $('.weather2').css({"display": "none"});
+            });
         return false;
     });
 
